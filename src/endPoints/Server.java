943020c5fc59381d5c88a4b890db23baed5fa1e1
Saurabh Shendye <Saurabh.shendye@gmail.com>
2017-06-04 02:11:28 -0600

    Server constructor done

--- src/endPoints/Server.java
+++ src/endPoints/Server.java
@@ -4,8 +4,30 @@
 
 package endPoints;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
 
-public class Server
+public class Server implements Runnable
 {
+    private int port;
+    private File path;
+    private ServerSocket serverSocket;
 
+    private Server(int port, File path) throws IOException
+    {
+        this.port = port;
+        this.path = path;
+        this.serverSocket = new ServerSocket(port,5);
+    }
+
+    public void run()
+    {
+
+    }
+
+    public static void main(String [] args)
+    {
+        //Server server = new Server();
+    }
 }
