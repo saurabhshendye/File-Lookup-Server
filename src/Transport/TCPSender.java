6eb025d33ca10748b200645a233e6babb58a1e03
Saurabh Shendye <Saurabh.shendye@gmail.com>
2017-06-06 13:15:02 -0600

    TCPSender reused from earlier project

--- src/Transport/TCPSender.java
+++ src/Transport/TCPSender.java
@@ -4,7 +4,26 @@
 
 package Transport;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class TCPSender
 {
+    private Socket socket;
+    private DataOutputStream dout;
 
+    public TCPSender(Socket S) throws IOException
+    {
+        this.socket = S;
+        dout = new DataOutputStream(socket.getOutputStream());
+    }
+
+    public synchronized void send_and_maintain(byte[] data_to_send) throws IOException
+    {
+        int D_len = data_to_send.length;
+        dout.writeInt(D_len);
+        dout.write(data_to_send, 0, D_len);
+        dout.flush();
+    }
 }
