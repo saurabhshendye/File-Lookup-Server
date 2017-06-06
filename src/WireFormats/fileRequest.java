c956d98f2c860307bcfe3747f14edbf4f274d5d7
Saurabh Shendye <Saurabh.shendye@gmail.com>
2017-06-06 12:51:58 -0600

    fileRequest class constructor done. getByteArray method partially done.

--- src/WireFormats/fileRequest.java
+++ src/WireFormats/fileRequest.java
@@ -5,7 +5,33 @@
 package WireFormats;
 
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public class fileRequest
 {
+    private String name;
 
+    public fileRequest(String fileName)
+    {
+        this.name = fileName;
+    }
+
+    public byte[] getByteArray() throws IOException
+    {
+        // This method creates a byte array which needs to be written onto the socket
+        ByteArrayOutputStream baopstream = new ByteArrayOutputStream();
+        DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(baopstream));
+
+
+
+        byte[] marshaled = baopstream.toByteArray();
+
+        baopstream.close();
+        dout.close();
+
+        return marshaled;
+    }
 }
