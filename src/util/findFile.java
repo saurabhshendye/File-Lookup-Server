ec842516e19278cf8fcd8aa18628aeef34374c8f
Saurabh Shendye <Saurabh.shendye@gmail.com>
2017-06-05 23:31:21 -0600

    Find file class added getPath method

--- src/util/findFile.java
+++ src/util/findFile.java
@@ -8,17 +8,42 @@ import java.io.File;
 
 public class findFile
 {
-    private File path;
+    private File path = null;
     private static final findFile ff = new findFile();
+    private static boolean flag = false;
+    private static File P;
 
     private findFile()
     {
 
     }
 
+    public synchronized void fileLookup(String name, File file)
+    {
+        File[] list = file.listFiles();
+        if(list!=null)
+            for (File fil : list)
+            {
+                if (fil.isDirectory())
+                {
+                    fileLookup(name, fil);
+                }
+                else if (name.equalsIgnoreCase(fil.getName()))
+                {
+                    P = fil.getParentFile();
+                    flag = true;
+                }
+            }
+    }
+
     public void setPath(File path)
     {
         this.path = path;
+    }
+
+    public File getPath()
+    {
+        return this.path;
     }
     public static findFile getInstance()
     {
