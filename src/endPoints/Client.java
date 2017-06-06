e9bb694051e3407c8804ecfaf36b6a560d7a8c32
Saurabh Shendye <Saurabh.shendye@gmail.com>
2017-06-06 12:46:53 -0600

    Updated client class

--- src/endPoints/Client.java
+++ src/endPoints/Client.java
@@ -4,10 +4,20 @@
 
 package endPoints;
 
+import util.clientArgParser;
+
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client
 {
-    public static void main(String [] args)
+    public static void main(String [] args) throws IOException
     {
+        clientArgParser parser = new clientArgParser(args);
+        if (parser.isValid())
+        {
+            Socket clientSocket = new Socket(parser.hostName, parser.port);
 
+        }
     }
 }
